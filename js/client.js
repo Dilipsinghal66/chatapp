a75a369f9502f825ae70de45c91c83501bab17e4
dilip <19102209@mail.jiit.ac.in>
2021-07-21 21:27:23 +0530

    m

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io(`//localhost:${PORT}`);
 
 const form = document.getElementById('send-container');
 const  messageinput = document.getElementById('messageinp');
